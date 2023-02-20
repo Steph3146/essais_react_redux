c14e2e6c1d87bb0872093c699b3b40df5c17eefa
STEPHANIE MALBERT <[STEPHANIE.MALBERT@live.fr](mailto:STEPHANIE.MALBERT@live.fr)>
2023-02-20 15:36:11 +0100

    Adding of the middleware

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-const redux = require('redux');
+import redux from 'redux'; 
+import reduxLogger from 'redux-logger';
+
+
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const logger = reduxLogger.createLogger();
+const applyMiddleware = redux.applyMiddleware;
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
-
-const BUY_CAKE = "BUY_CAKE";
-
-export default function buyCake() {
+// the buyCake function is the action creator
+function buyCake() {
     return {
         type: BUY_CAKE,
         info: 'First redux action'
+    }
+}
+
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM,
+        info: 'Second redux action'
     }
 }
 
@@ -16,23 +29,63 @@ export default function buyCake() {
 
 // ... s'appelle "spread operator". Sert à demander au reducer de faire une copie de l'objet state avant d'ajouter un gâteau au state initial. Certaines des propriétés vont rester inchangées.
 
-const initialState = {
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams : 20
+// }
+
+const initialCakeState = {
     numOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+    numOfIceCreams: 20
+}
+
+// const reducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case BUY_CAKE: return {
+//             ...state,
+//             numOfCakes: state.numOfCakes -1
+//         };
+//         case BUY_ICECREAM: return {
+//             ...state,
+//             numOfIceCreams: state.numOfIceCreams -1
+//         }
+//         default: return state
+//     }
+// }
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes -1
+        };
+        default: return state
+    }
+}
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams -1
         }
         default: return state
     }
 }
-const store = createStore(reducer);
-console.log("initialState", store.getState());
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+ });
+const store = createStore(rootReducer, applyMiddleware(logger));
+console.log('Initial state', store.getState());
 const unsubscribe = store.subscribe(() => console.log("Updated state", store.getState()));
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe();
